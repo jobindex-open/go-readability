0eaad8f84cc0171f795a4921a04834fc60ffeb8e
Mislav Marohnić <git@mislav.net>
2025-11-25 19:03:45 +0100

    Simplify README and update example usage

--- parser_test.go
+++ parser_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/url"
 	"os"
 	fp "path/filepath"
@@ -173,6 +174,23 @@ func Test_countCharsAndCommas(t *testing.T) {
 			}
 		})
 	}
+}
+
+func ExampleFromReader() {
+	srcFile, err := os.Open("index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer srcFile.Close()
+
+	baseURL, _ := url.Parse("https://example.com/path/to/article")
+	article, err := FromReader(srcFile, baseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Found article with title %q\n\n", article.Title())
+	article.RenderHTML(os.Stdout)
 }
 
 func extractSourceFile(path string) (Article, bool, *html.Node, error) {
